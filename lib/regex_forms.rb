8014bdea8c84c5495d5ca49b5b187825f4039f70
Guilherme Dantas Goes <guilhermedg@labcmi.org.br>
2024-01-03 15:22:00 -0300

    first commit

---
+++ lib/regex_forms.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "regex_forms/version"
+
+module RegexForms
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/regex_forms/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RegexForms
+  VERSION = "0.1.0"
+end
